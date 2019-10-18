538d86730245a68c4aaf410b9b66163b59488eef
SeemSach <52330734+SeemSach@users.noreply.github.com>
2019-10-17 21:34:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let front = document.querySelector('.front p');
+let back = document.querySelector('.back p');
+let card = document.querySelector('.card');
+
+document.querySelector('#btnPrev').addEventListener('click', prevCard);
+document.querySelector('#btnFlip').addEventListener('click', flipCard);
+document.querySelector('#btnNext').addEventListener('click', nextCard);
+
+
+
+
+let quiz = [];
+getQuiz();
+
+let currentCard = -1;
+
+function getQuiz() {
+    fetch('https://us-central1-firstapp-f11c1.cloudfunctions.net/newQuizData')
+    .then( resp => resp.json())
+    .then( respJson => {
+        quiz = respJson;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+function prevCard() {
+    card.classList.remove('flipped');
+    setTimeout(() => {
+        currentCard = currentCard - 1 < 0 ? currentCard : currentCard - 1;
+        front.textContent = quiz[currentCard].question;
+        back.textContent = quiz[currentCard].answer;
+    }, 500);
+    
+}
+
+function nextCard() {
+    card.classList.remove('flipped');
+    setTimeout(()=>{
+        currentCard = currentCard + 1 >= quiz.length ? currentCard : currentCard + 1;
+        front.textContent = quiz[currentCard].question;
+        back.textContent = quiz[currentCard].answer;        
+    }, 500);
+    
+}
+
+function flipCard() {
+    card.classList.toggle('flipped');
+}
